f3a10885008773d40f3b9ea81a5f58b71bac3eb6
marija <mmmfj92@gmail.com>
2022-12-21 19:43:10 +0100

    backend

--- config/config.js
+++ config/config.js
@@ -1,19 +1,22 @@
 import mongoose from "mongoose";
 
-
-const connection=async()=>{
+const url = "mongodb+srv://MarijaProject:a1s2d3f4g5h6j7k8i9@atlascluster.qwpqbug.mongodb.net/?retryWrites=true&w=majority"
+const connection =  async ()=>{
 
     try{
-        mongoose.connect("mongodb://localhost:27017",{ //link to our base
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
-
-    })
-}
-    catch(error){
-console.log(error.message);
+        const conn = await mongoose.connect(url,{
+            //must add in order to not get any error masseges:
+            useUnifiedTopology:true,
+            useNewUrlParser: true
+        })
+        console.log(`mongo database is connected!!! ${conn.connection.host} `)
+    }catch(error){
+        console.error(`Error: ${error} `)
+        process.exit(1) //passing 1 - will exit the proccess with error
     }
+
 }
 
 
 export default connection;
+
